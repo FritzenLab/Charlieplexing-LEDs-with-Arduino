69a03e7fad901cc6d85b6269f75bed60f8a429dc
Clovis Fritzen <clovisf@gmail.com>
2018-04-07 11:13:46 -0300

    added schematic and code
    added schematic and code

---
+++ Charlieplexing-Arduino-Sketch/Charlieplexing-Arduino-Sketch.ino
@@ -0,0 +1,96 @@
+
+
+void setup() {
+  // set the digital pin as output:
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+void loop() {
+
+    led1();
+    delay(100);
+    led5();
+    delay(100);
+    led2();
+    delay(100);
+    led6();
+    delay(100);
+    led3();
+    delay(100);
+    led7();
+    delay(100);
+    led4();
+    delay(100);
+    led8();
+    delay(100);
+  
+}
+
+void led1(){
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  digitalWrite(3, LOW);
+  digitalWrite(2, HIGH);  
+}
+void led2(){
+  pinMode(4, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(2, INPUT);
+  pinMode(5, INPUT);
+  digitalWrite(4, LOW);
+  digitalWrite(3, HIGH);  
+}
+void led3(){
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  digitalWrite(5, LOW);
+  digitalWrite(4, HIGH);  
+}
+void led4(){
+  pinMode(4, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, INPUT);
+  pinMode(5, INPUT);
+  digitalWrite(2, LOW);
+  digitalWrite(4, HIGH);  
+}
+void led5(){
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  digitalWrite(2, LOW);
+  digitalWrite(3, HIGH);  
+}
+void led6(){
+  pinMode(4, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(2, INPUT);
+  pinMode(5, INPUT);
+  digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);  
+}
+void led7(){
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);  
+}
+void led8(){
+  pinMode(4, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, INPUT);
+  pinMode(5, INPUT);
+  digitalWrite(4, LOW);
+  digitalWrite(2, HIGH);  
+}
+
